340034cd69354ffc8b362fc8fa066241899746f1
SebasBarretoOkorum <sebastian.barreto@okorum.com>
2025-12-16 21:45:45 -0500

    Fix marginBottom

--- backend/utils/docxGenerator.js
+++ backend/utils/docxGenerator.js
@@ -624,7 +624,7 @@ const tableRows = detalleData.map(
           margin: {
             top: 1900, 
             right: 800,
-            bottom: 2700,
+            bottom: 2800,
             left: 800,
           },
         },
